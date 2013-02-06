ed2176e1da79d3e3076114ec1eeae9f94a98541e
Mike Matranga <mike@dmgctrl.com>
2013-02-05 22:34:11 -0500

    Moved everything into the validator class.

--- ValidatorTest/Validator.m
+++ ValidatorTest/Validator.m
@@ -1,7 +1,33 @@
 #import "Validator.h"
-#import "StringFormatter.h"
 
-@implementation Validator
+@implementation Validator {
+    NSDictionary* _predefinedFormats;
+}
+
+
+# pragma mark - Init
+
+- (id) init
+{
+    if (nil != (self = [super init])) {
+        NSArray *usPhoneFormats = @[
+                                    @"011 $",
+                                    @"###-####",
+                                    @"(###) ###-####"
+                                    ];
+        
+        NSArray* usDateFormats = @[@"##/##/####"];
+        
+        _predefinedFormats = @{
+                              @"us": usPhoneFormats,
+                              @"us-date": usDateFormats
+                              };
+    }
+    return self;
+}
+
+
+# pragma mark - Key Value
 
 - (void) setValue:(id)object forKey:(NSString*)key
 {
@@ -16,24 +42,113 @@
     }
 }
 
+
+# pragma mark - Phone Number Validation
+
 - (void) _validatePhoneNumberField:(UITextField*)sender
 {
     if ([sender.text length] <= 14) {
-        StringFormatter* formatter = [[StringFormatter alloc] init];
-        [sender setText:[formatter format:sender.text withLocale:@"us"]];
+        [sender setText:[self _format:sender.text withLocale:@"us"]];
     } else {
         [sender setText:[sender.text substringToIndex:14]];
     }
 }
 
+
+# pragma mark - Date Validation
+
 - (void) _validateDateField:(UITextField*)sender
 {
     if ([sender.text length] <= 10) {
-        StringFormatter* formatter = [[StringFormatter alloc] init];
-        [sender setText:[formatter format:sender.text withLocale:@"us-date"]];
+        [sender setText:[self _format:sender.text withLocale:@"us-date"]];
     } else {
         [sender setText:[sender.text substringToIndex:10]];
     }
+}
+
+
+# pragma mark - Formatter
+
+- (NSString*) _format:(NSString*)inputString withLocale:(NSString*)locale
+{
+    NSArray *localeFormats = [_predefinedFormats objectForKey:locale];
+    if (localeFormats == nil) {
+        return inputString;
+    }
+    
+    NSString *input = [self _strip:inputString];
+    for (NSString *phoneFormat in localeFormats) {
+        int i = 0;
+        
+        NSMutableString *temp = [[NSMutableString alloc] init];
+        
+        for (int p = 0; temp != nil && i < [input length] && p < [phoneFormat length]; p++) {
+            char c = [phoneFormat characterAtIndex:p];
+            BOOL required = [self _canBeInputByNumberPad:c];
+            char next = [input characterAtIndex:i];
+            switch(c) {
+                case '$':
+                    p--;
+                    [temp appendFormat:@"%c", next]; i++;
+                    break;
+                case '#':
+                    if (next < '0' || next > '9') {
+                        temp = nil;
+                        break;
+                    }
+                    
+                    [temp appendFormat:@"%c", next]; i++;
+                    break;
+                default:
+                    if (required) {
+                        if (next != c) {
+                            temp = nil;
+                            break;
+                        }
+                        
+                        [temp appendFormat:@"%c", next]; i++;
+                    } else {
+                        [temp appendFormat:@"%c", c];
+                        if (next == c) {
+                            i++;
+                        }
+                    }
+                    break;
+            }
+        }
+        
+        if (i == [input length]) {
+            return temp;
+        }
+    }
+    return input;
+}
+
+
+# pragma mark - Formatter Helper
+
+- (NSString*) _strip:(NSString*)inputString
+{
+    NSMutableString *res = [[NSMutableString alloc] init];
+    for (int i = 0; i < [inputString length]; i++) {
+        char next = [inputString characterAtIndex:i];
+        if ([self _canBeInputByNumberPad:next]) {
+            [res appendFormat:@"%c", next];
+        }
+    }
+    return res;
+}
+
+- (BOOL) _canBeInputByNumberPad:(char)c
+{
+    if (c == '+' || c == '*' || c == '#') {
+        return YES;
+    }
+    
+    if (c >= '0' && c <= '9') {
+        return YES;
+    }
+    return NO;
 }
 
 @end
